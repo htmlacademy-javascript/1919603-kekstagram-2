c95eefa0e393f208c21aa86789c6d6380257028a
MiKuul <mixailpetrograd@gmail.com>
2025-10-11 13:17:10 +0300

    Доработка функций валидации

--- js/functions.js
+++ js/functions.js
@@ -1,30 +1,25 @@
-function stringLength (string, num) {
+function checkStringLength (string, maxLengthString) {
   const strLength = string.length;
-  return strLength <= num;
+  return strLength <= maxLengthString;
 }
 
 function isItPolindrom (str) {
-  const mainStr = str.replaceAll().toLowerCase();
-  let i = 0;
-  let newStr = '';
-  while (i < mainStr.length) {
-    newStr = `${mainStr[i]}${newStr}`;
-    i = i + 1;
-  }
-  console.log(newStr);
-  return mainStr === newStr;
+  const mainStr = str.replaceAll(' ', '').toLowerCase();
+  const reversedStr = mainStr.split('').reverse().join('');
+
+  return mainStr === reversedStr;
 }
 
 // Строка короче 4 символов
-console.log(stringLength('три', 4));
+console.log(checkStringLength('три', 4));
 // Строка равна 3 символам
-console.log(stringLength('три', 3));
+console.log(checkStringLength('три', 3));
 // Строка длиннее 2 символов
-console.log(stringLength('три', 2));
+console.log(checkStringLength('три', 2));
 
 // Строка является палиндромом
 console.log(isItPolindrom('топот'));
 // Несмотря на разный регистр, тоже палиндром
 console.log(isItPolindrom('ТоПот'));
 // Это не палиндром
-console.log(isItPolindrom('кекс'));
+console.log(isItPolindrom('кек кек'));
